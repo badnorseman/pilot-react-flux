04a9fa9e768355c50c09be81ee12ee6947554521
Dan Jensen <dj@fitbird.com>
2015-06-11 17:18:48 -0400

    divide products into elements

--- src/scripts/components/products/products.jsx
+++ src/scripts/components/products/products.jsx
@@ -3,7 +3,7 @@ var ProductStore = require("../../stores/product_store");
 var ProductActions = require("../../actions/product_actions");
 var Login = require("../login");
 var List = require("./product_list");
-// var AddItem = require("./product_add");
+var AddItem = require("./product_add");
 
 function getProductState() {
   return {
@@ -39,38 +39,6 @@ module.exports = React.createClass({
         </div>
       </div>
     );
-  }
-});
-
-var AddItem = React.createClass({
-  handleSubmit: function(e) {
-    e.preventDefault();
-
-    var name = React.findDOMNode(this.refs.name).value;
-    var description = React.findDOMNode(this.refs.description).value;
-
-    if (name && description) {
-      var record = {
-        product : {
-          name: name,
-          description: description
-        }
-      }
-      ProductActions.add(record);
-      React.findDOMNode(this.refs.name).value = "";
-      React.findDOMNode(this.refs.description).value = "";
-    }
-  },
-  render: function() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" ref="name" />
-          <input type="text" ref="description" />
-          <input type="submit" value="Add" />
-        </form>
-      </div>
-    )
   }
 });
 
