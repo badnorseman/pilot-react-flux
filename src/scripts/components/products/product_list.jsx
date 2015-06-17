def168e8a4de91f0a1e152d99dc3abc4a475d5d1
Dan Jensen <dj@fitbird.com>
2015-06-17 12:05:32 -0400

    update misc

--- src/scripts/components/products/product_list.jsx
+++ src/scripts/components/products/product_list.jsx
@@ -1,13 +1,11 @@
-import React from "react"
-import Item from "./product_list_item"
+import React from "react";
+import Item from "./product_list_item";
 
 export default class extends React.Component {
   render() {
-    let items = this.props.items.map(function(item) {
-      return(
+    let items = this.props.items.map(item => (
         <Item item={item} key={item.id} />
-      )
-    })
+    ))
 
     return(
       <div>
