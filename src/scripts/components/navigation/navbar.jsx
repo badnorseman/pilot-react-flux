72541b5c078b172896a4e3d228bb6ed5bd3e7365
Dan Jensen <dj@fitbird.com>
2015-06-19 11:30:02 -0400

    fix navbar for mobile

--- src/scripts/components/navigation/navbar.jsx
+++ src/scripts/components/navigation/navbar.jsx
@@ -3,6 +3,11 @@ import React from "react";
 export default class Navbar extends React.Component {
   constructor() {
     super()
+    this.toggle = this.toggle.bind(this)
+  }
+
+  toggle() {
+    $(".button-collapse").sideNav()
   }
 
   render() {
@@ -11,13 +16,13 @@ export default class Navbar extends React.Component {
         <nav>
           <div className="nav-wrapper">
             <a href="#!" className="brand-logo center">FitBird</a>
-            <a href="#" data-activates="mobile-nav" className="button-collapse">
+            <a href="#" data-activates="mobile-side-nav" className="button-collapse" onClick={this.toggle}>
               <i className="mdi-navigation-menu"></i></a>
             <ul className="right hide-on-med-and-down">
               <li><a href="">Log In</a></li>
               <li><a href="">Log Out</a></li>
             </ul>
-            <ul className="side-nav" id="mobile-nav">
+            <ul className="side-nav" id="mobile-side-nav">
               <li><a href="">Log In</a></li>
               <li><a href="">Log Out</a></li>
             </ul>
