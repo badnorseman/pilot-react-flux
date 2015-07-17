ff693e72035037abec3e3f32f4d3bc192637eef3
Dan Jensen <dj@fitbird.com>
2015-07-16 23:34:24 -0400

    update payment list

--- src/scripts/components/payments/payment_list_item.jsx
+++ src/scripts/components/payments/payment_list_item.jsx
@@ -13,18 +13,16 @@ export default class extends React.Component {
         <table className="mdl-data-table mdl-js-data-table">
           <thead>
             <tr>
+              <th>ORDER DATE</th>
               <th>PRICE</th>
-              <th>CURRENCY</th>
-              <th>TRANSACTION ID</th>
-              <th>TRANSACTION DATE</th>
+              <th>ORDER ID</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>{this.props.item.price}</td>
-              <td className="mdl-data-table__cell--non-numeric">{this.props.item.currency}</td>
-              <td className="mdl-data-table__cell--non-numeric">{this.props.item.transaction_id}</td>
               <td className="mdl-data-table__cell--non-numeric">{this.props.item.transaction_date}</td>
+              <td className="mdl-data-table__cell--non-numeric">{this.props.item.currency} {this.props.item.amount}</td>
+              <td className="mdl-data-table__cell--non-numeric">{this.props.item.transaction_id}</td>
             </tr>
           </tbody>
         </table>
