380ed0b8bc1802782f5d0ac0cc7bb5d3a8a84a8b
Dan Jensen <dj@fitbird.com>
2015-07-21 16:21:11 -0400

    update product list item

--- src/scripts/components/products/product_list_item.jsx
+++ src/scripts/components/products/product_list_item.jsx
@@ -10,6 +10,7 @@ export default class extends React.Component {
   render() {
     return(
       <div>
+        <Link to={`/products/${this.props.item.id}`} params={{id: this.props.item.id}}>
         <div className="mdl-cell mdl-cell--6-col-phone mdl-cell--4-col-tablet mdl-cell--3-col-desktop">
           <div className="mdl-card mdl-shadow--2dp">
             <div className="mdl-card__media">
@@ -19,16 +20,9 @@ export default class extends React.Component {
               <h6>{this.props.item.name}</h6>
               <p>{this.props.item.description}</p>
             </div>
-            <div className="mdl-card__actions text-center">
-              <Link
-                className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
-                to={`/products/${this.props.item.id}`}
-                params={{id: this.props.item.id}}>
-                Edit
-              </Link>
-            </div>
           </div>
         </div>
+        </Link>
       </div>
     )
   }
