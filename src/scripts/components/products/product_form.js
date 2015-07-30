276ef51c609302a6b9d97665af4ec5e6985700a4
Dan Jensen <dj@fitbird.com>
2015-07-30 18:00:18 -0400

    change isCurrency in product form

--- src/scripts/components/products/product_form.js
+++ src/scripts/components/products/product_form.js
@@ -44,7 +44,7 @@ export default class ProductForm extends React.Component {
   }
 
   _isCurrency(value) {
-    if (value === this.props.currency) { return true } else { return false }
+    return value === this.props.currency
   }
 
   render() {
