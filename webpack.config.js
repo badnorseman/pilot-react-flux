a9fef1c8aed48a60f70aca629160d1e4021d9ef7
Dan Jensen <dj@fitbird.com>
2015-08-04 09:10:32 -0400

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,21 +6,24 @@ module.exports = {
     "webpack/hot/dev-server",
     "./src/scripts/app.js"
   ],
-
   output: {
     path: __dirname + "/dist",
     filename: "bundle.js"
   },
-
-  module: {
-    loaders: [
-      { test: /\.jsx?$/, include: __dirname + "/src/scripts", loader: "babel-loader" },
-      { test: /\.css$/, loader: "style-loader!css-loader" },
-      { test: /\.(jpg|png)$/, loader: "url-loader?limit=8192" }
-    ]
-  },
-
   resolve: {
-    extensions: ["", ".js", ".jsx"]
+    extensions: ["", ".js"]
+  },
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      include: __dirname + "/src/scripts",
+      loader: "babel-loader"
+    }, {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+    }, {
+      test: /\.(jpg|png)$/,
+      loader: "url-loader?limit=8192"
+    }]
   }
 };
