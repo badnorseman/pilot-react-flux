ac15998d5ceefcd173a4f504af6724d4edc56712
Dan Jensen <dj@fitbird.com>
2015-08-06 23:51:58 -0400

    update action types

--- src/scripts/constants/action_types.js
+++ src/scripts/constants/action_types.js
@@ -1,15 +1,6 @@
 import keyMirror from "react/lib/keyMirror";
 
 export default keyMirror({
-  ADD_PRODUCT: null,
-  ADD_TRANSACTION: null,
-  EDIT_PRODUCT: null,
-  LIST_PRODUCT: null,
-  LIST_TRANSACTION: null,
-  LOGIN: null,
-  LOGOUT: null,
-  OAUTH: null,
-  REMOVE_PRODUCT: null,
   RECEIVE_CLIENT_TOKEN: null,
   RECEIVE_DATA_AUTH: null,
   RECEIVE_DATA_PRODUCT: null,
@@ -17,6 +8,5 @@ export default keyMirror({
   RECEIVE_ERRORS_AUTH: null,
   RECEIVE_ERRORS_PRODUCT: null,
   RECEIVE_ERRORS_TRANSACTION: null,
-  REQUEST_CLIENT_TOKEN: null,
-  SIGNUP: null
+  REQUEST_CLIENT_TOKEN: null
 })
--- src/scripts/dispatcher/dispatcher.js
+++ src/scripts/dispatcher/dispatcher.js
@@ -3,3 +3,21 @@ import { Dispatcher } from "flux";
 const flux = new Dispatcher();
 
 export default flux
+
+// export function register(callback) {
+//   return flux.register(callback);
+// }
+
+// export function dispatch(actionType, action = {}) {
+//   flux.dispatch({ actionType, ...action });
+// }
+
+// export function dispatchAsync(promise, actionTypes, action = {}) {
+//   const { request, success, failure } = actionTypes;
+//
+//   dispatch(request, action);
+//   promise.then(
+//     response => dispatch(success, { ...action, response }),
+//     error => dispatch(failure, { ...action, error })
+//   );
+// }
