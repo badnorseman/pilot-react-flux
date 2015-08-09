53f15b475507eeb32e17ade444c6f8450bfb290d
Dan Jensen <dj@fitbird.com>
2015-08-09 10:57:31 -0400

    clean up misc

--- src/scripts/components/main.js
+++ src/scripts/components/main.js
@@ -1,27 +1,24 @@
 "use strict";
-import React from "react";
+import React, { Component } from "react";
 import { Link, RouteHandler } from "react-router";
 import * as AuthActions from "../actions/auth_actions";
 import AuthStore from "../stores/auth_store";
 import Footer from "./footer";
 
-function _getStateFromStores() {
-  return {
-    isLoggedIn: AuthStore.isLoggedIn(),
-    user: AuthStore.getUser()
-  }
-}
-
-export default class Main extends React.Component {
+export default class Main extends Component {
   constructor() {
     super();
-    this.state = _getStateFromStores();
+    this.state = {
+      isLoggedIn: false,
+      user: {}
+    };
     this._handleLogout = this._handleLogout.bind(this);
     this._onChange = this._onChange.bind(this);
   }
 
   componentDidMount() {
     AuthStore.addChangeListener(this._onChange)
+    this.setState(this._getStateFromStores())
   }
 
   componentWillUnmount() {
@@ -32,8 +29,15 @@ export default class Main extends React.Component {
     componentHandler.upgradeDom()
   }
 
+  _getStateFromStores() {
+    return {
+      isLoggedIn: AuthStore.isLoggedIn(),
+      user: AuthStore.getUser()
+    }
+  }
+
   _onChange() {
-    this.setState(_getStateFromStores())
+    this.setState(this._getStateFromStores())
   }
 
   _handleLogout() {
